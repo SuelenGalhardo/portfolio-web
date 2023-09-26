fb249747d51bc4a0211653627d24e435d64cfc63
SuelenGalhardo <suelenranucci@gmail.com>
2023-09-26 12:24:22 +0200

    added scss sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { useState, } from 'react'
+//import { useState, } from 'react'
 //import { useState, useRef, useState } from 'react'
 import suelenLogo from './assets/logosuelen.png'
+
 import '../src/styles/main.scss';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
 <>
  <div className='header__container'>
+
         <header>
         <a href="https://github.com/SuelenGalhardo" target="_blank">
           <img src={suelenLogo} className="logo__suelen" alt="suelen logo" />
                   </a>
-    
-                
+                  <div></div>
+                  
      </header>
 
-      <h1>Suelen Galhardo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        </div>
+
+       
+        <main>
+
+
+        </main>
         <footer className='footer'>
           <small>
             <p className='footer__copy'>&copy; Suelen Galhardo</p>
