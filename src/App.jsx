7e470a19e30f6fa465128101532fd1128ec471ef
SuelenGalhardo <suelenranucci@gmail.com>
2024-01-11 16:09:33 +0100

    added confi bootstrap css jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,50 @@ function App() {
  <div className='header__container'>
 
         <header>
-        <a href="https://github.com/SuelenGalhardo" target="_blank">
+      
+                  <div>
+                    
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <a href="https://github.com/SuelenGalhardo" target="_blank">
           <img src={suelenLogo} className="logo__suelen" alt="suelen logo" />
                   </a>
-                  <div></div>
+    <a class="navbar-brand" href="#">
+      <img
+        src="./images/githubLogo.jpg"
+        alt=""
+        width="25"
+        class="d-inline-block align-text-top"
+      />
+      
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Intro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mis proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+                  </div>
                   
      </header>
 
