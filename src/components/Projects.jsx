867a2365305e762f62d61e3c7483df06f6814d3f
SuelenGalhardo <suelenranucci@gmail.com>
2024-01-27 11:30:45 +0100

    added proyects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,16 +12,16 @@ const Projects = () => {
     projectName: 'Anonymous Proxy',
     tech: ['HTML5', 'CSS3'],
     description:'Creación de una página web responsive basada en un diseño proporcionado.',
-    github: 'https://github.com/Lourdesjupo/webResponsive_example',
-    link: 'https://lourdesjupo.github.io/webResponsive_example/',
+    github: 'https://github.com/SuelenGalhardo/Anonymous-HTML-CSS',
+    link: 'https://beta.adalab.es/modulo-1-evaluacion-final-SuelenGalhardo/',
   },
   {
       img: Rick_and_Morty,
       projectName: 'Buscador Rick And Morty',
       tech: ['HTML5', 'SASS', 'ReactJS'],
       description:'Busca tus personajes favoritos de Rick and Morty y averigua su procedencia y otros datos interesantes.',
-      github: 'https://github.com/Lourdesjupo/Rick-and-Morty-Character-Finder',
-      link: 'https://lourdesjupo.github.io/Rick-and-Morty-Character-Finder/',
+      github: 'https://github.com/SuelenGalhardo/Rick-and-Morty',
+      link: 'https://suelengalhardo.github.io/Rick-and-Morty/',
     
   },
    {
@@ -29,8 +29,8 @@ const Projects = () => {
       projectName: 'Disney Character Database',
       tech: ['HTML5', 'SASS', 'JavaScript'],
       description:'Web Responsive donde almacenar todos tus personajes favoritos de Disney. ',
-      github: 'https://github.com/Lourdesjupo/Disney-Characters-Database',
-      link: 'https://lourdesjupo.github.io/Disney-Characters-Database/',
+      github: 'https://github.com/SuelenGalhardo/Disney-Character-Database',
+      link: 'https://suelengalhardo.github.io/Disney-Character-Database/',
    
     },
   
