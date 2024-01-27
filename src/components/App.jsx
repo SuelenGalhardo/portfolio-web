115b8734d431fba61bcd79861d85d483c1210f77
SuelenGalhardo <suelenranucci@gmail.com>
2024-01-27 12:55:05 +0100

    added proyects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,50 @@ import '../styles/App.scss';
 //import 'bootstrap/dist/css/bootstrap.min.scss';
 import Header from './Header';
 //import Main from './Main';
-import Projects from './Projects';
+import ProjectSection from './ProjectSection';
 import Footer from './Footer';
-
-
-
 //import { useState, } from 'react'
+import Anonymous from '../assets/Anonymous.png';
+import Disney_Character from '../assets/Disney.png'
+import Rick_and_Morty from '../assets/Rickmorty.png'
 //import { useState, useRef, useState } from 'react'
 
 /* The following line can be included in your src/index.js or App.js file */
 //import './App.scss';
 
 
-
+const arrayProjects= [
+  {
+    id:1,
+    img: Anonymous,
+    projectName: 'Anonymous Proxy',
+    tech: ['HTML5', 'CSS3'],
+    description:'Creación de una página web responsive basada en un diseño proporcionado.',
+    github: 'https://github.com/SuelenGalhardo/Anonymous-HTML-CSS',
+    link: 'https://beta.adalab.es/modulo-1-evaluacion-final-SuelenGalhardo/',
+  },
+  {
+      id:2,
+      img: Rick_and_Morty,
+      projectName: 'Buscador Rick And Morty',
+      tech: ['HTML5', 'SASS', 'ReactJS'],
+      description:'Busca tus personajes favoritos de Rick and Morty y averigua su procedencia y otros datos interesantes.',
+      github: 'https://github.com/SuelenGalhardo/Rick-and-Morty',
+      link: 'https://suelengalhardo.github.io/Rick-and-Morty/',
+    
+  },
+   {  id:3,
+      img: Disney_Character,
+      projectName: 'Disney Character Database',
+      tech: ['HTML5', 'SASS', 'JavaScript'],
+      description:'Web Responsive donde almacenar todos tus personajes favoritos de Disney. ',
+      github: 'https://github.com/SuelenGalhardo/Disney-Character-Database',
+      link: 'https://suelengalhardo.github.io/Disney-Character-Database/',
+   
+    },
+  
+    
+  ]
 
 function App() {
   
@@ -27,7 +58,9 @@ function App() {
 
         <Header/>
       
-        <Projects/>    
+        <ProjectSection
+        arrayProjects={arrayProjects}
+        />    
 
 
        
---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+
+
+const Projects = () => {
+
+  
+
+  
+  
+  return (
+
+    <section className={`projectSection ${hiddenClassProjects}`}>
+    <h3 className="projectSection__title">Projects</h3>
+    <section className="projectSection__container">
+      <button
+        className={`pagination__btn ${displayArrowL}`}
+        onClick={handlePrev}
+      >
+          );
+};
+
+
+export default Projects;
